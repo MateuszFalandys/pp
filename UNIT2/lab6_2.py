70ae812e1e4cbfa99e7792c2b15202f2f678ecbf
falandys <mateuszfalandys@gmail.com>
2023-01-21 10:20:37 +0100

    colos

---
+++ UNIT2/lab6_2.py
@@ -0,0 +1,5 @@
+print("Wykonam działania na 2 liczbach całkowitych.")
+a = int(input("Podaj pierwszą liczbę: \n"))
+b = int(input("Podaj pierwszą liczbę: \n"))
+print("Wyniki działania na liczbach ",a," oraz ",b," to:")
+print("Dla dodawania: ", a+b, "\n Dla odejmowanie: ", a-b, "\n Dla mnożenia: ", a*b, "\n Dla dzielenia: ", a/b, "\n Dla modulo: ", a%5b)
